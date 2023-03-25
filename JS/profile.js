5d2f937818890515d56f799599a165dd19a1a556
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-25 13:59:31 +0100

    added function outside of the event listener because dark mode has to be the whole time and not when the event of (change) of the checkbox happens, otherwise it will only remain with the color dark when the change happens, however this shit only works when the checkbox is unchecked, we need to figure it out why

---
+++ JS/profile.js
@@ -0,0 +1,8 @@
+// Adding Darkmode functionality
+const toogle = document.querySelector('[data-js="toogle"]');
+
+if (toogle.checked === true) {
+  document.body.style.backgroundColor = "#00005c";
+} else {
+  document.body.style.backgroundColor = "#00005c";
+}
