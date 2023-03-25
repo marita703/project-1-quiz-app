5a438ac13a36a1caeddcd36a7792c013d5c0dd91
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-25 21:34:28 +0100

    change the colors used in the previous styles to variables, and also put the event listener change in order to use the dark-mode, added an extra class to the body with all the new colors and it works, however is not a permanent change

--- JS/profile.js
+++ JS/profile.js
@@ -1,8 +1,10 @@
 // Adding Darkmode functionality
 const toogle = document.querySelector('[data-js="toogle"]');
 
-if (toogle.checked === true) {
-  document.body.style.backgroundColor = "#00005c";
-} else {
-  document.body.style.backgroundColor = "#00005c";
-}
+toogle.addEventListener("change", () => {
+  const checked = toogle.checked;
+
+  console.log(checked);
+
+  document.body.classList.toggle("dark-mode");
+});
