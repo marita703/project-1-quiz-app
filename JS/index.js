b500f1163e42d90c8e008df8df8e01b569cff413
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-27 10:44:21 +0200

    created an array of buttons and I console logged them and it works

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,15 @@
 // This is the code in order for the show answer button works.
 
 const buttons = document.querySelectorAll(".card__button");
+const 
 
 console.log(buttons);
+
+function showAnswer() {
+  console.log("I clicked");
+
+  body.all
+
+}
+
+buttons.forEach((buttons) => buttons.addEventListener("click", iClicked));
