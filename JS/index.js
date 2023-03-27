301b229db8d47f176808e8d0bd5e294e8d7e340c
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-27 10:19:20 +0200

    Added a new JS filefor the index, since working with only one JS files will cause interference

---
+++ JS/index.js
@@ -0,0 +1,5 @@
+// This is the code in order for the show answer button works.
+
+const buttons = document.querySelectorAll(".card__button");
+
+console.log(buttons);
--- JS/profile.js
+++ JS/profile.js
@@ -1,10 +1,32 @@
 // Adding Darkmode functionality
 const toogle = document.querySelector('[data-js="toogle"]');
 
+// This code I got it from youtube video: https://www.youtube.com/watch?v=Xk12JtYG8rw&ab_channel=EnvatoTuts%2B
+// it is modified to match our coding level
+
+function switchTheme() {
+  // get the Root emelemnt and the data-theme value
+
+  // This retrives the html tag
+  const rootElement = document.documentElement;
+
+  // This retrives the data-theme value that we set in HTML
+  let dataTheme = rootElement.getAttribute("data-theme");
+
+  let newTheme;
+
+  // if the data-theme is initialized with "light", then it will change to "dark"
+  // if not, then it will be initialized with "dark", and therfore change to "light"
+  if (dataTheme === "light") {
+    newTheme = "dark";
+  } else {
+    newTheme = "light";
+  }
+
+  // Set new attribute, that is "data-theme" to the new Theme
+  rootElement.setAttribute("data-theme", newTheme);
+}
+
 toogle.addEventListener("change", () => {
-  const checked = toogle.checked;
-
-  console.log(checked);
-
-  document.body.classList.toggle("dark-mode");
+  switchTheme();
 });
