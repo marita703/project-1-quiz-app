138b5ef069fca9359fed97737fac3d434e553ed5
Maria del Mar <chocoinformatics2016@gmail.com>
2023-03-27 12:51:14 +0200

    manage the bookmark to change to dark when I click

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,18 @@
+// This is the code for the toggle function:
+
+const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
+console.log(bookmarks);
+
+function toggleBookmark(event) {
+  console.log(event.target);
+  console.log(this.getAttribute("src"));
+  this.src = "./assets/bookmark_filled.png";
+}
+
+bookmarks.forEach((bookmarks) =>
+  bookmarks.addEventListener("click", toggleBookmark)
+);
+
 // This is the code in order for the show answer button works.
 
 const buttons = document.querySelectorAll(".card__button");
